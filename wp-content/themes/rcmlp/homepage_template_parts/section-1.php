4ad678cef595c329e5f62ee21bd3bc608721a480
Garrett Cullen <Garth619@users.noreply.github.com>
2018-08-11 18:01:21 -0700

    push

---
+++ wp-content/themes/rcmlp/homepage_template_parts/section-1.php
@@ -0,0 +1,46 @@
+<section id="section_one">
+	
+	<div class="logo">
+		
+		<img src="<?php bloginfo('template_directory');?>/images/logo.png"/>
+		
+	</div><!-- logo -->
+
+	<div class="orange_svg">
+		
+		<?php echo file_get_contents("wp-content/themes/rcmlp/images/orange.svg"); ?>
+		
+	</div><!-- orange_svg -->
+	
+	<div class="h1_wrapper">
+		
+		<div class="corner tl">
+			
+			<?php echo file_get_contents("wp-content/themes/rcmlp/images/corner.svg"); ?>
+			
+		</div><!-- corner -->
+		
+		<div class="corner tr">
+			
+			<?php echo file_get_contents("wp-content/themes/rcmlp/images/corner.svg"); ?>
+			
+		</div><!-- corner -->
+		
+		<h1>How do you compare to other dispensaries?</h1>
+		
+		<div class="corner bl">
+			
+			<?php echo file_get_contents("wp-content/themes/rcmlp/images/corner.svg"); ?>
+			
+		</div><!-- corner -->
+		
+		<div class="corner br">
+			
+			<?php echo file_get_contents("wp-content/themes/rcmlp/images/corner.svg"); ?>
+			
+		</div><!-- corner -->
+
+	
+	</div><!-- h1_wrapper -->
+	
+</section><!-- section_one -->
