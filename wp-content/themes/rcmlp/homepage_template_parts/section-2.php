b9dc63f9e1363c1eb3662133920baf2cdcfd9df7
Garrett Cullen <Garth619@users.noreply.github.com>
2018-08-11 18:36:31 -0700

    push

---
+++ wp-content/themes/rcmlp/homepage_template_parts/section-2.php
@@ -0,0 +1,11 @@
+<section id="section_two">
+	
+	
+	<div class="pinksvg">
+		
+		<?php echo file_get_contents("wp-content/themes/rcmlp/images/pink.svg"); ?>
+		
+	</div><!-- pinksvg -->
+	
+	
+</section><!-- section_two -->
