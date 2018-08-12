4ffb2438fe02d785972644879ea0ad3c48b3b882
Garrett Cullen <Garth619@users.noreply.github.com>
2018-08-12 12:46:13 -0700

    push

--- wp-content/themes/rcmlp/homepage_template_parts/section-2.php
+++ wp-content/themes/rcmlp/homepage_template_parts/section-2.php
@@ -1,14 +1,53 @@
 <section id="section_two">
 	
-	
-
-	
-	
-		<div class="pinksvg">
+	<div class="pinksvg">
 		
 			<?php echo file_get_contents("wp-content/themes/rcmlp/images/pink-weed.svg"); ?>
 		
 		</div><!-- pinksvg -->
+		
+		<div class="sec_two_inner">
+			
+			
+				<div class="sec_two_header">
+					
+					
+					<h2>Let us share your score with you!</h2>
+					
+					<div class="blue_line_wrapper">
+						
+						<div class="circle"></div>
+						
+						<div class="blue_line"></div><!-- blue_line -->
+						
+					</div><!-- blue_line_wrapper -->
+					
+				</div><!-- sec_two_header -->
+			
+				<div class="iphone">
+					
+					<div class="iphone_meta">
+						
+						<div class="date">
+							
+							<?php
+							
+							date_default_timezone_set('America/Los_Angeles');
+							echo date("h:i A");
+							
+							?>
+							
+						</div><!-- date -->
+						
+					</div><!-- iphone_meta -->
+			
+			
+			
+				</div><!-- iphone -->
+			
+		</div><!-- sec_two_inner -->
+		
+	
 	
 	
 		<div class="light_grey_wrapper"></div><!-- light_grey_wrapper -->
